63363456b5813f7d917844a5ef2affe5741bc58f
BenoitPE <benoitpegaz@gmail.com>
2021-10-01 11:20:25 +0200

    Adding content to MainActivity

--- app/src/main/java/com/example/projetandroid/MainActivity.kt
+++ app/src/main/java/com/example/projetandroid/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.projetandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.projetandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
